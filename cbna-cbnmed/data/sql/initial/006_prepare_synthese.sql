64e615630efd2f8d97d92e98b35028b1ac928d51
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-02-23 15:23:18 +0100

    Feat cbna-cbnmed: use reports.field, move SQL function in shared

--- cbna-cbnmed/data/sql/initial/006_prepare_synthese.sql
+++ cbna-cbnmed/data/sql/initial/006_prepare_synthese.sql
@@ -4,27 +4,6 @@
 BEGIN ;
 
 
-\echo '-------------------------------------------------------------------------------'
-\echo 'Set function "get_id_source_by_name()"'
-CREATE OR REPLACE FUNCTION gn_synthese.get_id_source_by_name(sourceName character varying)
-    RETURNS integer
-    LANGUAGE plpgsql
-    IMMUTABLE
-AS
-$function$
-    -- Function which return the id_source from an name_source
-    DECLARE idSource integer;
-
-    BEGIN
-        SELECT INTO idSource id_source
-        FROM gn_synthese.t_sources AS ts
-        WHERE ts.name_source = sourceName ;
-
-        RETURN idSource ;
-    END;
-$function$ ;
-
-
 \echo '----------------------------------------------------------------------------'
 \echo 'Delete previous data loaded in synthese from this sources'
 DELETE FROM gn_synthese.synthese
