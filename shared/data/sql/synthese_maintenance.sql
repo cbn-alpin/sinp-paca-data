70ec2d7334b4292dcaa6eb50e13d8a0bbc37b672
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-08-10 15:34:40 +0200

    Feat shared: simplify synthese maintenance SQL script

--- shared/data/sql/synthese_maintenance.sql
+++ shared/data/sql/synthese_maintenance.sql
@@ -1,21 +1,20 @@
-\echo 'Maintenance on gn_synthese tables after massive insert into synthese'
+\echo 'Maintenance on gn_synthese tables after massive insert into synthese.'
+\echo 'Perform VACUUM FULL VERBOSE ANALYSE on multiple tables that will lock up.'
 \echo 'Rights: superuser'
 \echo 'GeoNature database compatibility : v2.3.0+'
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Maintenance on "synthese"'
-VACUUM FULL VERBOSE gn_synthese.synthese ;
-ANALYSE VERBOSE gn_synthese.synthese ;
+\echo 'Maintenance on "synthese" => table locked !'
+VACUUM FULL VERBOSE ANALYSE gn_synthese.synthese ;
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Maintenance on "cor_area_synthese"'
-VACUUM FULL VERBOSE gn_synthese.cor_area_synthese ;
-ANALYSE VERBOSE gn_synthese.cor_area_synthese ;
+\echo 'Maintenance on "cor_area_synthese" => table locked !'
+VACUUM FULL VERBOSE ANALYSE gn_synthese.cor_area_synthese ;
 
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'For GeoNature < v2.6.0, maintenance on "cor_area_taxon"'
+\echo 'For GeoNature < v2.6.0, maintenance on "cor_area_taxon" => table locked !'
 DO $$
     BEGIN
         IF EXISTS (
@@ -24,8 +23,7 @@ DO $$
             WHERE table_schema = 'gn_synthese'
                 AND table_name = 'cor_area_taxon'
         ) IS TRUE THEN
-            VACUUM FULL VERBOSE gn_synthese.cor_area_taxon ;
-            ANALYSE VERBOSE gn_synthese.cor_area_taxon ;
+            VACUUM FULL VERBOSE ANALYSE gn_synthese.cor_area_taxon ;
         ELSE
       		RAISE NOTICE ' GeoNature > v2.5.5 => table "gn_synthese.cor_area_taxon" not exists !' ;
         END IF ;
