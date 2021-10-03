275284034934a9a7a58b349ee395c8af60aa5705
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-10-03 15:24:13 +0200

    Feat shared: remove FULL from VACUUM, useless with recent Postgres version

--- shared/data/sql/source_maintenance.sql
+++ shared/data/sql/source_maintenance.sql
@@ -1,8 +1,12 @@
 \echo 'Maintenance on gn_synthese.t_sources tables after massive insert, update or delete.'
-\echo 'Perform VACUUM FULL VERBOSE ANALYSE on table that will lock up.'
+\echo 'Perform VACUUM VERBOSE ANALYSE on table that will NOT be lock up.'
 \echo 'Rights: dbowner'
 \echo 'GeoNature database compatibility : v2.3.0+'
 
 \echo '-------------------------------------------------------------------------------'
-\echo 'Maintenance on "gn_synthese.t_sources" => table locked !'
-VACUUM FULL VERBOSE ANALYSE gn_synthese.t_sources ;
+\echo 'Maintenance on "gn_synthese.t_sources"'
+VACUUM VERBOSE ANALYSE gn_synthese.t_sources ;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Reindex "gn_synthese.t_sources"'
+REINDEX TABLE gn_synthese.t_sources ;
