89e77b2dfa425d27d0351ef65f01dc6c5d69a0ce
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-03-08 14:45:40 +0100

    Feat cbna: add update scipt

--- cbna/README.md
+++ cbna/README.md
@@ -1,20 +1,52 @@
-# CBNA DATA
+# CBNMED DATA
 
-## Import donnée de test
 
-* Au prélable, créer localement une base de données GeoNature à l'aide du script `install_db.sh` de GeoNature.
-* Copier/coller le fichier `shared/settings.example.ini` en le renomant `shared/settings.ini`.
+## Préparation de l'archive
+
+### Vérification de l'archive
+
+Avant de lancer le script d'import initial ou de mise à jour, il est nécessaire
+de s'assurer de l'intégrité du format CSV de l'archive et de la possibilité de
+pouvoir intégrer les données dans la base (abscence de doublon sur les index uniques).
+Pour cela :
+* dézipper l'archive reçu
+* appliquer les commandes listées sur la page [Outils et commandes utiles pour les imports](https://wiki-sinp.cbn-alpin.fr/database/utilitaires-imports) pour vérifier :
+    * le nombre de tabulations de chaque ligne
+    * la présence de ligne en doublon
+    * la presence de doublon pour la colonne UUID
+
+### Création de l'archive sur Dropbox
+
+L'archive au format d'échange doit être stocké sur Dropbox au niveau du dossier
+`Applications/data-paca-sinp/cbna`.
+Elle doit être compressé au format `.tar.bz2` pour cela se placer dans le
+dossier contenant les fichiers `.csv` du format d'échange et lancer la commande :
+```
+tar jcvf ../2022-03-15_sinp_paca_cbna.tar.bz2 .
+```
+
+## Import des données
+
+* Si nécessaire, copier/coller le fichier `shared/config/settings.sample.ini`
+en le renomant `shared/config/settings.ini`.
     * Modifier dans ce fichier les paramètres de connexion à votre base de données GeoNature.
-* Copier/coller le fichier `config/settings.sample.ini` en le renomant `config/settings.ini`.
-    * Modifier dans ce fichier les paramètres déjà présent s'ils ne correspondent pas à vos besoins. Si nécessaire, vous pouvez aussi y surcharger des paramètres du fichier  `config/settings.default.ini`.
-* Utiliser le script `bin/import_initial.sh` pour importer le jeu de données du CBNA.
-    * La sauvegarde (fichier .bak) de la flore globale du CBNA sera téléchargée depuis Dropbox.
-    * La sauvegarde sera restoré pour servir de base à l'import.
-    * Les scripts SQL du dossier `cbna/data/sql/initial/` vont être exécuté séquentiellement pour réaliser l'import.
+* Si nécessaire, copier/coller le fichier `cbna/config/settings.sample.ini`
+en le renomant `cbna/config/settings.ini`.
+    * Adapter à votre installation les paramètres présents dans ce fichier. Si
+    nécessaire, vous pouvez aussi y surcharger des paramètres du fichier
+    `cbna/config/settings.default.ini`.
+* Se placer dans le dossier `cbna/bin/` et utiliser le script :
+    *`./import_update.sh -v` pour mettre à jour les données du CBNA.
+        * Le script se charge de télécharger les données brutes depuis Dropbox
+        * Les scripts SQL du dossier `shared/data/sql/update/` seront ensuite
+        exécutés séquentiellement dans ordre déterminé pour la mise à jour.
+    * l'import initial a été fait via les script présent dans `cbna-cbnmed/bin/`
+
 
 ## Synchronisation serveur
 
-Pour transférer uniquement le dossier `cbna/` sur le serveur, utiliser `rsync` en testant avec l'option `--dry-run` (à supprimer quand tout est ok):
+Pour transférer uniquement le dossier `cbna/` sur le serveur, utiliser `rsync`
+en testant avec l'option `--dry-run` (à supprimer quand tout est ok):
 
 ```
 rsync -av --copy-unsafe-links --exclude var --exclude .gitignore --exclude settings.ini --exclude "data/raw/*" ./ geonat@db-paca-sinp:~/data/cbna/ --dry-run
---
+++ cbna/bin/import_update.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+# Encoding : UTF-8
+# Import some updated, deleted or new data in GeoNature Database the CBNA Flora Data.
+
+#+----------------------------------------------------------------------------------------------------------+
+# Configure script execute options
+set -euo pipefail
+
+# DESC: Usage help
+# ARGS: None
+# OUTS: None
+function printScriptUsage() {
+    cat << EOF
+Usage: ./$(basename $BASH_SOURCE)[options]
+     -h | --help: display this help
+     -v | --verbose: display more infos
+     -x | --debug: display debug script infos
+     -c | --config: path to config file to use (default : config/settings.ini)
+EOF
+    exit 0
+}
+
+# DESC: Parameter parser
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: Variables indicating command-line parameters and options
+function parseScriptOptions() {
+    # Transform long options to short ones
+    for arg in "${@}"; do
+        shift
+        case "${arg}" in
+            "--help") set -- "${@}" "-h" ;;
+            "--verbose") set -- "${@}" "-v" ;;
+            "--debug") set -- "${@}" "-x" ;;
+            "--config") set -- "${@}" "-c" ;;
+            "--"*) exitScript "ERROR : parameter '${arg}' invalid ! Use -h option to know more." 1 ;;
+            *) set -- "${@}" "${arg}"
+        esac
+    done
+
+    while getopts "hvxc:" option; do
+        case "${option}" in
+            "h") printScriptUsage ;;
+            "v") readonly verbose=true ;;
+            "x") readonly debug=true; set -x ;;
+            "c") setting_file_path="${OPTARG}" ;;
+            *) exitScript "ERROR : parameter invalid ! Use -h option to know more." 1 ;;
+        esac
+    done
+}
+
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+function main() {
+    #+----------------------------------------------------------------------------------------------------------+
+    # Load utils
+    source "$(dirname "${BASH_SOURCE[0]}")/../../shared/lib/utils.bash"
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Init script
+    initScript "${@}"
+    parseScriptOptions "${@}"
+    loadScriptConfig "${setting_file_path-}"
+    redirectOutput "${cbna_log_imports}"
+    checkSuperuser
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Start script
+    printInfo "${app_name} script started at: ${fmt_time_start}"
+
+    downloadCenpacaDataArchive
+    extractArchive
+    prepareDb
+
+    buildTablePrefix
+
+    parseCsv "source" "so"
+    executeCopy "source"
+    displayStats "source"
+    executeUpgradeScript "source" "insert"
+    executeUpgradeScript "source" "update"
+
+    parseCsv "organism" "o"
+    executeCopy "organism"
+    displayStats "organism"
+    executeUpgradeScript "organism" "insert"
+    executeUpgradeScript "organism" "update"
+
+    parseCsv "user" "u"
+    executeCopy "user"
+    displayStats "user"
+    executeUpgradeScript "user" "insert"
+    executeUpgradeScript "user" "update"
+
+    parseCsv "acquisition framework" "af"
+    executeCopy "acquisition framework"
+    displayStats "acquisition framework"
+    executeUpgradeScript "acquisition framework" "insert"
+    executeUpgradeScript "acquisition framework" "update"
+
+    parseCsv "dataset" "d"
+    executeCopy "dataset"
+    displayStats "dataset"
+    executeUpgradeScript "dataset" "insert"
+    executeUpgradeScript "dataset" "update"
+
+    parseCsv "synthese" "s"
+    executeCopy "synthese"
+    displayStats "synthese"
+    executeUpgradeScript "synthese" "insert"
+    executeUpgradeScript "synthese" "update"
+    executeUpgradeScript "synthese" "delete"
+
+    executeUpgradeScript "dataset" "delete"
+    executeUpgradeScript "acquisition framework" "delete"
+    executeUpgradeScript "user" "delete"
+    executeUpgradeScript "organism" "delete"
+    executeUpgradeScript "source" "delete"
+
+    reloadCorAreaSynthese
+
+    printPretty "Are you sure to run maintain DB SQL script wich lock database (y/n). Default: n ?" ${Red}
+    read -r -n 1 key
+    echo # Move to a new line
+    if [[ "${key}" =~ ^[Yy]$ ]]; then
+        maintainDb
+    fi
+
+    #+----------------------------------------------------------------------------------------------------------+
+    # Display script execution infos
+    displayTimeElapsed
+}
+
+function downloadCenpacaDataArchive() {
+    printMsg "Download CBNA data archive..."
+
+    if [[ ! -f "${raw_dir}/${cbna_filename_archive}" ]]; then
+        curl -X POST https://content.dropboxapi.com/2/files/download \
+            --header "Authorization: Bearer ${cbna_dropbox_token}" \
+            --header "Dropbox-API-Arg: {\"path\": \"${cbna_dropbox_dir}/${cbna_filename_archive}\"}" \
+            > "${raw_dir}/${cbna_filename_archive}"
+     else
+        printVerbose "Archive file \"${cbna_filename_archive}\" already downloaded." ${Gra}
+    fi
+}
+
+function extractArchive() {
+    printMsg "Extract import data CSV files..."
+
+    if [[ -f "${raw_dir}/${cbna_filename_archive}" ]]; then
+        if [[ ! -f "${raw_dir}/${cbna_filename_synthese}" ]]; then
+            cd "${raw_dir}/"
+            tar jxvf "${raw_dir}/${cbna_filename_archive}"
+        else
+            printVerbose "CSV files already extracted." ${Gra}
+        fi
+    fi
+}
+
+function prepareDb() {
+    printMsg "Insert utils functions into GeoNature database..."
+    export PGPASSWORD="${db_pass}"; \
+        psql -h "${db_host}" -U "${db_user}" -d "${db_name}" \
+            -f "${sql_shared_dir}/utils_functions.sql"
+}
+
+function buildTablePrefix() {
+    table_prefix="${app_code}_${cbna_import_date//-/}"
+}
+
+function parseCsv() {
+    if [[ $# -lt 2 ]]; then
+        exitScript "Missing required argument to ${FUNCNAME[0]}()!" 2
+    fi
+
+    local type="${1,,}"
+    local type_abbr="${2,,}"
+
+    local data_type="${type// /_}"
+    local data_type_abbr=$(echo "${type}" | sed 's/\(.\)[^ ]* */\1/g')
+    if [[ "${#data_type_abbr}" = "1" ]]; then
+        data_type_abbr="${data_type}"
+    fi
+    declare -n csv_file="cbna_filename_${data_type_abbr}"
+    local csv_to_import="${csv_file%.csv}_rti.csv"
+
+    # Exit if CSV file not found
+    if ! [[ -f "${raw_dir}/${csv_file}" ]]; then
+        return 0
+    fi
+
+    printMsg "Parse ${type^^} CSV file..."
+    if [[ ! -f "${raw_dir}/${csv_to_import}" ]]; then
+        cd "${root_dir}/import-parser/"
+        pipenv run python ./bin/gn_import_parser.py \
+            --type "${type_abbr}" \
+            --config "${conf_dir}/parser_actions_update.ini" \
+            "${raw_dir}/${csv_file}"
+    else
+        printVerbose "${type^^} CSV file already parsed." ${Gra}
+    fi
+}
+
+function executeCopy() {
+    if [[ $# -lt 1 ]]; then
+        exitScript "Missing required argument to ${FUNCNAME[0]}()!" 2
+    fi
+
+    local type="${1,,}"
+
+    local data_type="${type// /_}"
+    local data_type_abbr=$(echo "${type}" | sed 's/\(.\)[^ ]* */\1/g')
+    if [[ "${#data_type_abbr}" = "1" ]]; then
+        data_type_abbr="${data_type}"
+    fi
+    declare -n csv_file="cbna_filename_${data_type_abbr}"
+    local csv_to_import="${csv_file%.csv}_rti.csv"
+
+    # Exit if CSV file not found
+    if ! [[ -f "${raw_dir}/${csv_file}" ]]; then
+        return 0
+    fi
+
+    local table="${table_prefix}_${data_type}"
+    local sql_file="${sql_shared_dir}/update/${data_type}/copy.sql"
+    local psql_var="${data_type_abbr}ImportTable"
+
+    printMsg "Copy ${type^^} in GeoNature database..."
+    checkSuperuser
+    sudo -n -u "${pg_admin_name}" -s \
+        psql -d "${db_name}" \
+            -v "${psql_var}=${table}" \
+            -v gnDbOwner="${db_user}" \
+            -v csvFilePath="${raw_dir}/${csv_to_import}" \
+            -f "${sql_file}"
+}
+
+function displayStats() {
+    if [[ $# -lt 1 ]]; then
+        exitScript "Missing required argument to ${FUNCNAME[0]}()!" 2
+    fi
+
+    local type="${1,,}"
+
+    local data_type="${type// /_}"
+    local table="${table_prefix}_${data_type}"
+    local data_type="${type// /_}"
+    local data_type_abbr=$(echo "${type}" | sed 's/\(.\)[^ ]* */\1/g')
+    if [[ "${#data_type_abbr}" = "1" ]]; then
+        data_type_abbr="${data_type}"
+    fi
+    declare -n csv_file="cbna_filename_${data_type_abbr}"
+
+    # Exit if CSV file not found
+    if ! [[ -f "${raw_dir}/${csv_file}" ]]; then
+        return 0
+    fi
+
+    printMsg "Display ${type^^} stats..."
+    export PGPASSWORD="${db_pass}"; \
+        psql -h "${db_host}" -U "${db_user}" -d "${db_name}" \
+            -v importTable="${table}" \
+            -f "${sql_shared_dir}/update/stats.sql"
+
+}
+
+function executeUpgradeScript() {
+    if [[ $# -lt 2 ]]; then
+        exitScript "Missing required argument to ${FUNCNAME[0]}()!" 2
+    fi
+
+    local type="${1,,}"
+    local action="${2,,}"
+
+    local data_type="${type// /_}"
+    local data_type_abbr=$(echo "${type}" | sed 's/\(.\)[^ ]* */\1/g')
+    if [[ "${#data_type_abbr}" = "1" ]]; then
+        data_type_abbr="${data_type}"
+    fi
+    declare -n csv_file="cbna_filename_${data_type_abbr}"
+
+    # Exit if CSV file not found
+    if ! [[ -f "${raw_dir}/${csv_file}" ]]; then
+        return 0
+    fi
+
+    local table="${table_prefix}_${data_type}"
+    local sql_file="${sql_shared_dir}/update/${data_type}/${action}.sql"
+    local psql_var="${data_type_abbr}ImportTable"
+
+    printMsg "${action^} ${type^^} in destination table..."
+    export PGPASSWORD="${db_pass}"; \
+        sed "s/\${${psql_var}}/${table}/g" "${sql_file}" | \
+        psql -h "${db_host}" -U "${db_user}" -d "${db_name}" \
+            -v "${psql_var}=${table}" \
+            -f -
+}
+
+function reloadCorAreaSynthese() {
+    printMsg "Reload cor_area_synthese table..."
+
+    checkSuperuser
+    export PGPASSWORD="${db_pass}"; \
+        psql -h "${db_host}" -U "${db_user}" -d "${db_name}" \
+            -f "${sql_shared_dir}/reload_cor_area_synthese.sql"
+}
+
+function maintainDb() {
+    printMsg "Executing database maintenance on updated tables..."
+
+    checkSuperuser
+    sudo -n -u "${pg_admin_name}" -s \
+        psql -d "${db_name}" \
+            -f "${sql_shared_dir}/synthese_maintenance.sql"
+}
+
+main "${@}"
