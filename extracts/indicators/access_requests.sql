ca844e6926fe7618b23ce55f8d10a54d44131059
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-01-09 15:46:31 +0100

    feat(extract/indicators): speed up the query of access requests

--- extracts/indicators/access_requests.sql
+++ extracts/indicators/access_requests.sql
@@ -3,31 +3,10 @@
 -- - <db-user-pwd> : replace with the database user password.
 \timing off
 COPY (
-    SELECT
-        ro.nom_role AS nom,
-        ro.prenom_role AS prenom,
-        o.nom_organisme AS nom_organisme,
-        re.additional_data ->> 'userOrganism' AS nom_organisme_demande,
-        re.additional_data ->> 'projectType' AS type_etude,
-        re.additional_data ->> 'projectDescription' AS desc_etude,
-        re.additional_data ->> 'kingdom' AS regne_concerne,
-        re.sensitive_access AS acces_donnees_senssible,
-        to_char(re.meta_create_date, 'DD/MM/YYYY') AS date_demande,
-        to_char(re.processed_date, 'DD/MM/YYYY') AS date_traitement,
-        CASE re.processed_state
-            WHEN 'refused' THEN 'refusé'
-            WHEN 'accepted' THEN 'accepté'
-            ELSE ''
-        END AS etat,
-        re.refusal_reason AS raison_refus,
-        to_char(re.end_date, 'DD/MM/YYYY') AS limite_temporelle,
-        (
-            SELECT string_agg(DISTINCT (area_name), ', ' ORDER BY area_name ASC)
-            FROM ref_geo.l_areas
-            WHERE id_area::TEXT = ANY (regexp_split_to_array(re.geographic_filter, ','))
-        ) AS limites_geographiques,
-        (
-            SELECT string_agg(
+    WITH limit_taxo AS (
+        SELECT
+            id_request,
+            string_agg(
                 DISTINCT (
                     CASE
                         WHEN regne = 'Plantae' AND group1_inpn = 'Bryophytes'
@@ -38,20 +17,88 @@ COPY (
                             THEN 'Algues'
                         WHEN regne = 'Fungi'
                             THEN 'Fonge'
-                        WHEN regne = 'Animalia' AND phylum IN ('Arthropoda', 'Annelida', 'Cnidaria', 'Mollusca', 'Platyhelminthes')
+                        WHEN regne = 'Animalia' AND phylum IN (
+                            'Arthropoda', 'Annelida', 'Cnidaria', 'Mollusca', 'Platyhelminthes'
+                        )
                             THEN 'Invertébré'
                         WHEN regne = 'Animalia' AND phylum = 'Chordata'
                             THEN 'Vertébré'
+                        WHEN regne = 'Plantae' AND group1_inpn = 'Autres'
+                            THEN 'Plantae'
+                        WHEN regne = 'Animalia' AND phylum is null
+                            THEN 'Animalia'
                     END
-                ), ', ')
-            FROM taxonomie.taxref
-            WHERE cd_nom::TEXT = ANY (regexp_split_to_array(re.taxonomic_filter, ','))
-        ) AS limites_taxonomiques
+                ), ', ') AS limites_taxonomiques
+        FROM (
+                SELECT
+                    t1.id_request,
+                    t1.cd,t.regne,
+                    t.phylum,
+                    t.group1_inpn
+                FROM (
+                        select
+                            id_request,
+                            unnest(regexp_split_to_array(re.taxonomic_filter, ','))::integer AS cd
+                        FROM gn_permissions.t_requests AS re
+                        WHERE re.taxonomic_filter IS NOT NULL
+                            AND re.taxonomic_filter != ''
+                    ) AS t1
+                    JOIN taxonomie.taxref AS t
+                        ON t.cd_nom = t1.cd
+            ) AS t2
+        GROUP BY id_request
+    ),
+    limit_geo AS (
+        SELECT
+            id_request,
+            string_agg(DISTINCT (area_name), ', ' ORDER BY area_name ASC) AS limites_geographiques
+        FROM (
+                SELECT
+                    t1.id_request,
+                    t1.id_area,
+                    la.area_name
+                FROM (
+                        SELECT
+                            id_request,
+                            unnest(regexp_split_to_array(re.geographic_filter, ','))::integer AS id_area
+                        FROM gn_permissions.t_requests AS re
+                        WHERE re.geographic_filter IS NOT NULL
+                            AND re.geographic_filter != ''
+                    ) AS t1
+                    JOIN ref_geo.l_areas AS la
+                        ON la.id_area = t1.id_area
+            ) AS t2
+        GROUP BY id_request
+    )
+    SELECT
+        ro.nom_role AS nom,
+        ro.prenom_role AS prenom,
+        o.nom_organisme AS nom_organisme,
+        re.additional_data ->> 'userOrganism' AS nom_organisme_demande,
+        re.additional_data ->> 'projectType' AS type_etude,
+        regexp_replace(re.additional_data ->> 'projectDescription', E'[\\n\\r]+', ' ', 'g' ) AS desc_etude,
+        re.additional_data ->> 'kingdom' AS regne_concerne,
+        re.sensitive_access AS acces_donnees_sensibles,
+        to_char(re.meta_create_date, 'DD/MM/YYYY') AS date_demande,
+        to_char(re.processed_date, 'DD/MM/YYYY') AS date_traitement,
+        CASE re.processed_state
+            WHEN 'refused' THEN 'refusé'
+            WHEN 'accepted' THEN 'accepté'
+            ELSE ''
+        END AS etat,
+        regexp_replace(re.refusal_reason, E'[\\n\\r]+', ' ', 'g' ) AS raison_refus,
+        to_char(re.end_date, 'DD/MM/YYYY') AS limite_temporelle,
+        lg.limites_geographiques,
+        lt.limites_taxonomiques
     FROM gn_permissions.t_requests AS re
         LEFT JOIN utilisateurs.t_roles AS ro
             ON re.id_role = ro.id_role
         LEFT JOIN utilisateurs.bib_organismes AS o
             ON ro.id_organisme = o.id_organisme
+        LEFT JOIN limit_taxo AS lt
+            ON re.id_request = lt.id_request
+        LEFT JOIN limit_geo AS lg
+            ON re.id_request = lg.id_request
     ORDER BY re.processed_date ASC
 ) TO stdout
 WITH (format csv, header, delimiter E'\t') ;
