34e71c01836446d36dc9525bff0f08dd24c0052c
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-02-07 14:52:53 +0100

    fix(cenpaca): SQL syntax errors

--- cenpaca/data/sql/fix/008_fix_duplicate_keywords.sql
+++ cenpaca/data/sql/fix/008_fix_duplicate_keywords.sql
@@ -1,33 +1,33 @@
-\ echo 'Fix duplicate keyword faune in datasets table.'
-\ echo 'Rights: db-owner'
-\ echo 'GeoNature database compatibility : v2.6.2+'
+\echo 'Fix duplicate keyword faune in datasets table.'
+\echo 'Rights: db-owner'
+\echo 'GeoNature database compatibility : v2.6.2+'
 -- Usage: psql -h "localhost" -U "<db-owner-name>" -d "<db-name>" -f <path-to-this-sql-file>
 -- Ex.: psql -h "localhost" -U "geonatadmin" -d "geonature2db" -f ~/data/cenpaca/data/sql/fix/008_*
 BEGIN;
 
-\ echo '----------------------------------------------------------------------------'
-\ echo 'Updating datasets keywords...'
+\echo '----------------------------------------------------------------------------'
+\echo 'Updating datasets keywords...'
 
 UPDATE gn_meta.t_datasets AS d
 SET keywords = unduplicate.keywords
 FROM (
-	SELECT
-	    td.id_dataset,
-	    td.keywords AS origin,
-	    (
-	        SELECT array_to_string(
-	            ARRAY(
-	                SELECT DISTINCT trim(x)
-	                FROM unnest(string_to_array(td.keywords, ',')) AS x
-	            ),
-	            ', '
-	        )
-	    ) AS keywords
-	FROM gn_meta.t_datasets AS td
-	WHERE td.keywords ILIKE '%faune%'
+    SELECT
+        td.id_dataset,
+        td.keywords AS origin,
+        (
+            SELECT array_to_string(
+                ARRAY(
+                    SELECT DISTINCT trim(x)
+                    FROM unnest(string_to_array(td.keywords, ',')) AS x
+                ),
+                ', '
+            )
+        ) AS keywords
+    FROM gn_meta.t_datasets AS td
+WHERE td.keywords ILIKE '%faune%'
 ) AS unduplicate
 WHERE d.id_dataset = unduplicate.id_dataset ;
 
-\ echo '----------------------------------------------------------------------------'
-\ echo 'COMMIT if all is ok:'
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all is ok:'
 COMMIT;
