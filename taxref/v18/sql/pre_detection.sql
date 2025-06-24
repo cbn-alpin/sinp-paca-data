a522b2084fd5add6f87a7e927f81f6bc7353119c
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-06-24 10:35:23 +0200

    refactor(taxref): add comments to v18 pre detection script

--- taxref/v18/sql/pre_detection.sql
+++ taxref/v18/sql/pre_detection.sql
@@ -7,12 +7,18 @@ ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_meta_dates_change_synthese 
 -- Disable trigger "tri_update_calculate_sensitivity"
 ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_update_calculate_sensitivity ;
 
+-- -------------------------------------------------------------------------------------------------
+-- Manage synthese
+
 UPDATE gn_synthese.synthese
 SET cd_nom = NULL
 WHERE cd_nom IN (138395);
 
 UPDATE gn_synthese.synthese SET cd_nom = 57077 WHERE cd_nom = 658461 ;
 
+
+-- -------------------------------------------------------------------------------------------------
+-- Manage bib_noms
 
 DELETE FROM taxonomie.cor_nom_liste AS l
 WHERE l.id_nom IN (
@@ -27,8 +33,10 @@ WHERE cd_nom IN (138395) ;
 UPDATE taxonomie.bib_noms SET cd_nom = 57077 WHERE cd_nom = 658461 ;
 
 
-UPDATE taxonomie.t_sensitivity_rules SET cd_nom = 138121 WHERE cd_nom = 718726 ;
+-- -------------------------------------------------------------------------------------------------
+-- Manage t_sensitivity_rules
 
+UPDATE taxonomie.t_sensitivity_rules SET cd_nom = 138121 WHERE cd_nom = 718726 ;
 
 UPDATE taxonomie.t_sensitivity_rules SET cd_nom = 124412 WHERE cd_nom = 124413 ;
 
