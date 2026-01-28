900fd3bbad9e8a179b689ab974a13b7c26882bc8
Jean-Michel GENIS <61179734+jmg-cbna@users.noreply.github.com>
2026-01-28 14:50:32 +0100

    Create observations_count_by_departments_filter_last_five_years.sql

---
+++ extracts/indicators/observations_count_by_departments_filter_last_five_years.sql
@@ -0,0 +1,49 @@
+--nombre d'observations par départements, par règne pour les 5 dernières années, hors annéee en cours
+WITH kingdoms_counts AS (
+        select
+        date_part('year',s.date_max ) as annee,
+            la.area_code,
+            la.area_name,
+            t.regne AS kingdom,
+            COUNT(s.id_synthese) AS obs_nbr
+        FROM gn_synthese.synthese AS s
+            JOIN gn_synthese.cor_area_synthese AS cas
+                ON s.id_synthese = cas.id_synthese
+            JOIN ref_geo.l_areas AS la
+                ON la.id_area = cas.id_area
+            JOIN ref_geo.bib_areas_types AS bat
+                ON bat.id_type = la.id_type
+            JOIN taxonomie.taxref AS t
+                ON t.cd_nom = s.cd_nom
+        WHERE bat.type_code = 'DEP' and date_part('year',s.date_max ) >= date_part('year',current_date) - 6 and date_part('year',s.date_max ) < date_part('year',current_date)
+        GROUP BY date_part('year',s.date_max ),la.area_code, la.area_name, t.regne
+    )
+    select
+    	annee,
+        area_code AS code_dept,
+        area_name AS dept,
+        kingdom AS regne,
+        obs_nbr AS obs_nbre
+    FROM (
+        select
+        	annee
+            area_code,
+            area_name,
+            kingdom,
+            obs_nbr,
+            0 AS sort_order
+        FROM kingdoms_counts
+
+        UNION
+
+        select
+        	kc.annee,
+            kc.area_code,
+            kc.area_name,
+            'Total' AS kingdom,
+            SUM(kc.obs_nbr) AS obs_nbr,
+            1 AS sort_order
+        FROM kingdoms_counts AS kc
+        GROUP BY kc.annee,kc.area_code, kc.area_name
+    ) AS counts_and_total
+    ORDER BY annee,area_code, sort_order, kingdom;
