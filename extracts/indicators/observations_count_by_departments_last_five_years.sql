2a90101433fed1c80631cbc24fdc793ccd728844
yolanrichard <yolan.richard@gmail.com>
2026-01-28 21:44:36 +0100

    Update observations_count_by_departments_last_five_years.sql

--- extracts/indicators/observations_count_by_departments_last_five_years.sql
+++ extracts/indicators/observations_count_by_departments_last_five_years.sql
@@ -1,7 +1,6 @@
 --- nombre d'observations par département et par règne pour les 5 dernières années auf année en cours
 WITH kingdoms_counts AS (
         select
-
             la.area_code,
             la.area_name,
             t.regne AS kingdom,
@@ -19,14 +18,12 @@ WITH kingdoms_counts AS (
         GROUP BY la.area_code, la.area_name, t.regne
     )
     select
-
         area_code AS code_dept,
         area_name AS dept,
         kingdom AS regne,
         obs_nbr AS obs_nbre
     FROM (
         select
-
             area_code,
             area_name,
             kingdom,
@@ -37,7 +34,6 @@ WITH kingdoms_counts AS (
         UNION
 
         select
-
             kc.area_code,
             kc.area_name,
             'Total' AS kingdom,
@@ -46,4 +42,4 @@ WITH kingdoms_counts AS (
         FROM kingdoms_counts AS kc
         GROUP BY kc.area_code, kc.area_name
     ) AS counts_and_total
-    ORDER BY area_code, sort_order, kingdom;
+    ORDER BY area_code, sort_order, kingdom
